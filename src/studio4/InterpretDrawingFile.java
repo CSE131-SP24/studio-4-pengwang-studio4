5bad6bd04d65a3be71b8d1fb481790055aac71ba
jejpeng <jejpeng@localhost>
2024-02-21 15:33:00 -0600

    done !

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,56 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		StdDraw.setPenColor(red,green,blue);
+		
+		
+		boolean isFilled = in.nextBoolean();
+
+		
+		if(shapeType.equals("rectangle")) {
+			double parameterOne = in.nextDouble();
+			double parameterTwo = in.nextDouble();
+			double parameterThree = in.nextDouble();
+			double parameterFour = in.nextDouble();
+			
+			if(isFilled == true) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}else {
+				StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}else if(shapeType.equals("ellipse")) {
+			double parameterOne = in.nextDouble();
+			double parameterTwo = in.nextDouble();
+			double parameterThree = in.nextDouble();
+			double parameterFour = in.nextDouble();
+
+			if(isFilled == true) {
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}else {
+				StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}else if(shapeType.equals("triangle")) {
+			double parameterOne = in.nextDouble();
+			double parameterTwo = in.nextDouble();
+			double parameterThree = in.nextDouble();
+			double parameterFour = in.nextDouble();
+			double parameterFive = in.nextDouble();
+			double parameterSix = in.nextDouble();
+			
+			double xParameters[] = {parameterOne, parameterThree, parameterFive};
+			double yParameters[] = {parameterTwo, parameterFour, parameterSix};
+			
+			if(isFilled == true) {
+				StdDraw.filledPolygon(xParameters, yParameters);
+			}else {
+				StdDraw.polygon(xParameters, yParameters);
+			}
+ 		}
+		
+		
 	}
 }
